f8d03cd164bd77b7fda92ea5f6dea398c73e03dd
Dimitar Nonov <d.nonov@gmail.com>
2018-07-06 19:09:56 +0100

    Add more test code

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,14 @@
 import "../styles/style.less";
-import _ from "lodash";
+// import _ from "lodash";
 import small from"../media/2.png";
 import big from "../media/1.jpg";
 
 function add(a, b) {
 	return a+b;
 }
-
-const compacted = _.compact([0,1,false,]);
-console.log(compacted);
+// console.log("horosan");
+// const compacted = _.compact([0,1,false,]);
+// console.log(compacted)
 
 
 const img = document.createElement("img");
